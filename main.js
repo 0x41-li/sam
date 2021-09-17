a8df801c7eb5a9255734070ef24e379cef88e0c8
Ali Sbane <ali.sbane.dev@gmail.com>
2021-09-17 15:17:06 +0100

    Pagination implementation on progress

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let top250movies = null;
+let moviesLefted = [];
 let moviesResult = document.querySelector(".movies-result-row");
+let scrollEventAllow = true;
 
 fetch("test-api.json")
   .then((response) => {
@@ -7,44 +9,53 @@ fetch("test-api.json")
   })
   .then((data) => {
     top250movies = data.items;
-    top250movies.forEach((movie, key) => {
-      if (key < 10) {
-        //create a div
-        let movieCol = document.createElement("div");
-
-        // adding class to the above div
-        movieCol.className = "movie-box col-sm-6 col-md-4 col-lg-3";
-
-        // apending the div to the parent div
-        moviesResult.appendChild(movieCol);
-
-        // showing the image of the movie
-        let imageDiv = document.createElement("div");
-        imageDiv.className = "movie-image img-loading"; // adding class to this div
-        movieCol.appendChild(imageDiv); // appending the image div to the movie column
-
-        let imageEl = document.createElement("img"); // create img element
-        imageEl.setAttribute("loading", "lazy");
-        imageEl.className = "img-fluid object-fit-contain img-loading";
-        imageEl.src = movie.image; // adding the source of the movie image
-        imageDiv.appendChild(imageEl); // appending the image element
-
-        imageEl.style.opacity = "0";
-
-        imageEl.addEventListener("load", () => {
-          imageEl.classList.add("img-loaded");
-          imageEl.style.opacity = "1";
-          imageDiv.classList.remove("img-loading");
-        });
-
-        movieCol.addEventListener("click", movieModalInfo.bind(this, movie));
-      }
-    });
+    showMovies(top250movies);
   })
   .catch((err) => {
     console.error(err);
   });
 
+// The Modal for every movie
 function movieModalInfo(movieInfo) {
   console.log(movieInfo);
+}
+
+// show movies on the page
+function showMovies(movies) {
+  movies.forEach((movie, key) => {
+    if (key < 10) {
+      //create a div
+      let movieCol = document.createElement("div");
+
+      // adding class to the above div
+      movieCol.className = "movie-box col-sm-6 col-md-4 col-lg-3";
+
+      // apending the div to the parent div
+      moviesResult.appendChild(movieCol);
+
+      // showing the image of the movie
+      let imageDiv = document.createElement("div");
+      imageDiv.className = "movie-image img-loading"; // adding class to this div
+      movieCol.appendChild(imageDiv); // appending the image div to the movie column
+
+      load(movie.image).then(() => {
+        imageDiv.style.backgroundImage = "url(" + movie.image + ")";
+        imageDiv.className = "movie-image";
+      });
+
+      movieCol.addEventListener("click", movieModalInfo.bind(this, movie));
+    } else {
+      moviesLefted.push(movie);
+    }
+  });
+}
+
+// Code for checking if image loaded
+function load(src) {
+  return new Promise(function (resolve, reject) {
+    const image = new Image();
+    image.addEventListener("load", resolve);
+    image.addEventListener("error", reject);
+    image.src = src;
+  });
 }
