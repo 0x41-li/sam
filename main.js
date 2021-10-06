de0621e3c71294ac27872eab6ff1a82c749f2f05
Ali Sbane <ali.sbane.dev@gmail.com>
2021-10-06 13:47:42 +0100

    Adding pagination functionality with load more button, and modal functionality

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 let top250movies = null;
-let moviesLefted = [];
 let moviesResult = document.querySelector(".movies-result-row");
-let scrollEventAllow = true;
+let loadMoreBtn = document.getElementById("load-more-btn");
+let modal = new bootstrap.Modal(document.getElementById("movie-modal"), {
+  keyboard: false,
+});
+
+let modalTitle = document.getElementById("modal-title");
+let modalBody = document.getElementById("modal-body");
+
 
 fetch("test-api.json")
   .then((response) => {
@@ -9,53 +15,92 @@ fetch("test-api.json")
   })
   .then((data) => {
     top250movies = data.items;
-    showMovies(top250movies);
+    showMovies();
   })
   .catch((err) => {
     console.error(err);
   });
 
+// load more click event
+loadMoreBtn.addEventListener("click", showMovies);
+
 // The Modal for every movie
-function movieModalInfo(movieInfo) {
-  console.log(movieInfo);
+function movieModal(movie) {
+  console.log(movie);
+  modal.toggle();
+  modalTitle.textContent = movie.title;
+  modalBody.innerHTML = `
+  <div class="fluid-container">
+    <div class="row">
+      <div class="col-12 col-lg-4 mb-4 mb-lg-0">
+        <div class="modal-image-movie movie-image img-loading cursor-pointer">
+          <img class="img-fluid w-100" src="${movie.image}" alt="">
+        </div>
+      </div>
+      <div class="col">
+        <div class="movie-info">
+          <h2 class="mb-4">${movie.fullTitle}<h2>
+          <p class="fs-5 fw-normal">Year published: <span class="fw-light text-secondary">${movie.year}</span></p>
+          <p class="fs-5 fw-normal">Crew: <span class="fw-light text-secondary">${movie.crew}</span></p>
+          <p class="fs-5 fw-normal">Rate: <span class="fw-light text-secondary">${movie.imDbRating}</span></p>
+          <p class="fs-5 fw-normal">Rate Count: <span class="fw-light text-secondary">${movie.imDbRatingCount}</span></p>
+          <p class="fs-5 fw-normal">Rank: <span class="fw-light text-secondary">${movie.rank}</span></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
 }
 
 // show movies on the page
-function showMovies(movies) {
-  movies.forEach((movie, key) => {
+function showMovies() {
+  top250movies.forEach((movie, key) => {
     if (key < 10) {
       //create a div
       let movieCol = document.createElement("div");
 
-      // adding class to the above div
+      // adding classes to the above div
       movieCol.className = "movie-box col-sm-6 col-md-4 col-lg-3";
 
-      // apending the div to the parent div
+      movieCol.setAttribute("data-toggle", "modal");
+      movieCol.setAttribute("role", "button");
+      movieCol.setAttribute("data-target", "#movie-modal");
+
+      // add the div to the parent div
       moviesResult.appendChild(movieCol);
 
-      // showing the image of the movie
+      // create a wrapper div for the image element
       let imageDiv = document.createElement("div");
-      imageDiv.className = "movie-image img-loading"; // adding class to this div
-      movieCol.appendChild(imageDiv); // appending the image div to the movie column
 
-      load(movie.image).then(() => {
-        imageDiv.style.backgroundImage = "url(" + movie.image + ")";
-        imageDiv.className = "movie-image";
+      // adding classes to this div
+      imageDiv.className = "movie-image img-loading";
+
+      // create image element
+      let imgElement = document.createElement("img");
+
+      // Appending elements to the DOM
+      movieCol.appendChild(imageDiv);
+      imageDiv.appendChild(imgElement);
+
+      // if so add it to the relative img's src attribute
+      imgElement.src = movie.image;
+
+      imgElement.style.opacity = "0";
+
+      // check if any image has been load
+      imgElement.addEventListener("load", () => {
+        // check if the images are rendered by checking the natural height and width
+        if (imgElement.naturalHeight > 0 && imgElement.naturalWidth > 0) {
+          imgElement.style.opacity = "1";
+          imageDiv.className = "movie-image";
+        }
       });
 
-      movieCol.addEventListener("click", movieModalInfo.bind(this, movie));
-    } else {
-      moviesLefted.push(movie);
+      movieCol.addEventListener("click", movieModal.bind(this, movie));
     }
   });
-}
 
-// Code for checking if image loaded
-function load(src) {
-  return new Promise(function (resolve, reject) {
-    const image = new Image();
-    image.addEventListener("load", resolve);
-    image.addEventListener("error", reject);
-    image.src = src;
-  });
+  for (let i = 0; i < 10; i++) {
+    top250movies.shift();
+  }
 }
